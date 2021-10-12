539429b8a23990fbd44faf919861f5779f61a0a9
Rupinder Kaushal <89014750+Rupinderkaushal@users.noreply.github.com>
2021-10-12 16:52:37 +0530

    done

--- src/Components/tablecol/Tcollapse.js
+++ src/Components/tablecol/Tcollapse.js
@@ -1,11 +1,38 @@
-import { Table,Row,Col,Button } from 'antd';
+import { Table,Row,Col,Button,Modal } from 'antd';
 import Chik  from "../../chik.png";
 import React from 'react';
 import Box from "../../box.png";
 import "../tablecol/tb.css";
 import {useEffect,useState} from "react"
+import { fontWeight } from '@mui/system';
+
 function NestedTable() {
+    const [visible,setVisible]= useState(false);
+        const DisplayModal =()=>{
+            setVisible(true);
+        };
+        const ok =()=>{
+            setVisible(false);
+        };
+        const cancel =()=>{
+            setVisible(false);
+        }
+    const [isModalVisible, setIsModalVisible] = useState(false);
+   
+  
+    const showModal = () => {
+      setIsModalVisible(true);
+  };
+  
+  const handleOk = () => {
+      setIsModalVisible(false);
+  };
+  
+  const handleCancel = () => {
+      setIsModalVisible(false);
+  };
   const expandedRowRender = () => {
+      
     const styLE ={
  
       border:"solid",borderRadius: '25px',width:"119px",height:"47px", border:"1px solid  #1083FF", textAlign:"center",margin:'0px 3px'
@@ -105,6 +132,34 @@ function NestedTable() {
               )
           }
       },
+      {
+          title:'',
+          render :()=>{
+             return(
+                <div>
+                <button onClick={DisplayModal}  style={{fontSize:'13px'}} className='btn btn-primary'>Enter</button>
+                <Modal visible={visible} onOk={ok} onCancel={cancel} >
+      <div style={{width:'400px',padding:'20px 10px'}}  >
+          <h5 style={{textAlign:'center'}}>Transaction confirmation</h5>
+          <p><span style={{fontWeight:'bold'}}>Current Price </span><br/>
+          0.5$
+          </p>
+          <p><span style={{fontWeight:'bold'}}>ESTIMATED TX FEE </span> <br/>
+          $0.00 USD
+          </p>
+          <p> <span style={{fontWeight:'bold'}}>ESTIMATED TOTAL </span> <br/>
+          $0.00 USD
+          </p>
+         <div className='d-flex justify-content-around'>
+         <button style={{background:'white',color:"blue",width:'100px',height:'30px',borderRadius:'5px',border:'1px solid blue'}}>Btn 1</button>
+         <button style={{background:'blue',color:"white",width:'100px',height:'30px',borderRadius:'5px',border:'none'}} >Btn 2</button>
+         </div>
+      </div>
+                </Modal>
+            </div>
+             )
+          }
+      }
   ]
   const data= [
       {
@@ -126,7 +181,6 @@ function NestedTable() {
           key:'3'
               },
   ]
-
   
    
     return(
@@ -136,21 +190,43 @@ function NestedTable() {
             </Row>
             <Row style={ROW} >
                 <Col offset={1} style={LanE} span={2.5}>Open Lane</Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>2</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>3</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>4</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>5</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>6</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>7</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>2</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>3</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>4</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>5</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>6</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>7</label></Button></Col>
                
             </Row>
           <Row  style={{paddingBottom:"30px"}}>
-          <Col offset={8} span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>8</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>9</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>10</label></Button></Col>
-                <Col  span={2.5} ><Button id="btn-btn" style={styLE} ><label style={Lable}>11</label></Button></Col>
+          <Col offset={8} span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>8</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>9</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>10</label></Button></Col>
+                <Col  span={2.5} ><Button onClick={showModal} id="btn-btn" style={styLE} ><label style={Lable}>11</label></Button></Col>
           </Row>
-         <Table  scroll={{x:400}} columns={columns} dataSource={data} pagination={false} />
+         <Table   scroll={{x:400}} columns={columns} dataSource={data} pagination={false} 
+          
+          
+         />
+          <Modal   visible={isModalVisible} onOk={handleOk}  >
+              <Row span={24}>
+            <Col offset={6} span={12} >
+            <h4 style={{textAlign:'center'}}>Juvinile Jubile</h4>
+            </Col>
+             <Col offset={4}  style={{textAlign:'center'}}  span={2} >
+             <button onClick={handleCancel} style={{background:'transparent',border:'none',fontSize:'30px'}}><i class="fa fa-times" aria-hidden="true"></i></button>
+             </Col>
+              </Row>
+              <Row className='d-flex justify-content-around'>
+                  <Col  ><label style={{fontWeight:'bolder',fontSize:'18px'}}>Packing Order :<span style={{background:'black',color:'white',padding:'5px 15px',borderRadius:'5px',marginLeft:'10px'}}>A</span></label></Col>
+                  <Col><input style={{width:'300px',height:'40px',textAlign:'center',borderRadius:'30px',border:'1px solid #449EFF'}}  placeholder="holla"></input></Col>
+                  <Col><button style={{ background: "#DD5859", color: 'white',border:'none',padding:'10px 25px',borderRadius:'5px'}}> <span><i class="fa fa-sliders" aria-hidden="true"></i></span> Filter</button></Col>
+              </Row>
+          <Table   scroll={{x:400}} columns={columns} dataSource={data} pagination={false} 
+          
+          
+          />
+                                </Modal>
       </div>
     );
   };
@@ -336,12 +412,13 @@ const columns = [
         scroll={{x:400}}
         id='tabb'
       columns={columns}
+    
       expandRowByClick={true}
       expandable={{ expandedRowRender }}
     //   onExpand={onTableRowExpand}
       dataSource={data}
       pagination={false}
-      rowKey={(record) => record.key}
+    //   rowKey={(record) => record.key}
     />
     </div>
   );
